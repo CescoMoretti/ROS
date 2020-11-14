0c59e26531df0541fc938205ab268589c91df4e2
CescoMoretti <cesco.moretti@gmail.com>
2020-11-14 12:09:01 +0100

    change nackroung in back_to_square

--- Es2_autoturtle/src/autoturtle/src/back_to_square_one.py
+++ Es2_autoturtle/src/autoturtle/src/back_to_square_one.py
@@ -6,6 +6,7 @@ from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
 from turtlesim.srv import TeleportAbsolute
 from turtlesim.srv import SetPen
+from std_srvs.srv import Empty
 import time
 
 
@@ -22,7 +23,7 @@ class ControlTurtlesim():
                 teleport_absolute = rospy.ServiceProxy('/turtle1/teleport_absolute',TeleportAbsolute)
                 teleport_absolute(x, y, theta)                    
             except rospy.ServiceException,e:    
-                print "Service call failed: %s" %e
+                print "Teleport: Service call failed: %s" %e
         #implement set pen
         def set_pen(flag):
             rospy.wait_for_service('/turtle1/set_pen')
@@ -30,7 +31,22 @@ class ControlTurtlesim():
                 set_pen = rospy.ServiceProxy('/turtle1/set_pen', SetPen)
                 set_pen(250,230,250,3,flag)
             except rospy.ServiceException,e:    
-                print "Service call failed: %s" %e
+                print "Set pen color: Service call failed: %s" %e
+        #implement change color of background
+        def change_background(r,g,b):
+            rospy.wait_for_service('/clear')
+            try: 
+                print "test"
+                rospy.set_param('/turtlesim/background_r', r)
+                rospy.set_param('/turtlesim/background_g', g)
+                rospy.set_param('/turtlesim/background_b', b)                
+                change_background_color = rospy.ServiceProxy('/clear', Empty)
+                change_background_color()
+                
+            except rospy.ServiceException,e:
+                print "Change background color: Service call failed : %s" %e
+
+
         #implement pose
         rospy.init_node('ControlTurtlesim', anonymous=False)             
         self.pose_subscriber = rospy.Subscriber('/turtle1/pose',Pose, self.update_pose)
@@ -39,11 +55,13 @@ class ControlTurtlesim():
         rospy.loginfo(" Press CTRL+c to stop moving the Turtle")       
         rospy.on_shutdown(self.shutdown)        
         #msg publisher
-        self.cmd_vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)        
+        self.cmd_vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=40)        
         move_cmd = Twist()
         #starting data 
-        l = input("insert square lenght: ")        
+        l = input("insert square lenght: ")
         
+        change_background(255,70,70)        
+ 
         # set frequency to 1/x second	   
         x = 40
         rate = rospy.Rate(x);        
@@ -76,12 +94,13 @@ class ControlTurtlesim():
             if round(self.pose.x,ndigits) == 1 and round(self.pose.y,ndigits) ==1 and lastFlag:                
                 teleport_absolute_client(self.pose.x,self.pose.y,math.pi*2)
                 cycleFlag = False
-
+            
             self.cmd_vel.publish(move_cmd)
             rate.sleep()
+        change_background(69,86,255) #neded it make consecutive test, but it delete the previus square
     #shutdown function
     def shutdown(self):        
-        rospy.loginfo("Stopping the turtle")
+        rospy.loginfo("Stopping the turtle")        
         self.cmd_vel.publish(Twist())        
         rospy.sleep(1)
 #launch for node
