2f25d4380fc52b92b2efc27a142837a18d2e34a3
CescoMoretti <cesco.moretti@gmail.com>
2021-04-30 16:44:19 +0200

    verione 1.0

--- ws_lezioni/src/beatle_test/src/test_lidar.py
+++ ws_lezioni/src/beatle_test/src/test_lidar.py
@@ -1,6 +1,6 @@
-!/usr/bin/env python
+#!/usr/bin/env python
 import rospy
-from stensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
      
