79c7ef24952bcb4b3c9a02963ad382a4d44fea73
pc_lab <pc_lab@gmail.com>
2021-05-07 08:52:18 +0200

    tolto build e devel

--- ws_lezioni/build/follow_the_gap/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cescom/Scrivania/progetti/ROS/ws_lezioni/src/follow_the_gap
-# Build directory: /home/cescom/Scrivania/progetti/ROS/ws_lezioni/build/follow_the_gap
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ws_lezioni/devel/share/wall_following/cmake/wall_following-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(wall_following_MSG_INCLUDE_DIRS "/home/cescom/Scrivania/progetti/ROS/ws_lezioni/src/wall_following/msg")
-set(wall_following_MSG_DEPENDENCIES sensor_msgs)
